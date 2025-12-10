e123875e3eda0c13139c0daec05fe4937ada78f8
Ranses Jiménez Domínguez <hiperiom412@gmail.com>
2025-12-10 18:41:10 -0400

    Agregar estructura inicial de la aplicación con política de privacidad, términos de servicio y plantillas de correo electrónico - Se crearon archivos Markdown para la política de privacidad y los términos de servicio. - Se añadió el diseño principal de la aplicación en una plantilla Blade. - Se implementó la plantilla de correo electrónico para invitación de equipos. - Se configuró Tailwind CSS con fuentes personalizadas y plugins. - Se desarrollaron pruebas de características para permisos de tokens de API, autenticación y gestión de cuentas de usuario. - Se añadieron pruebas para verificación de correo electrónico, confirmación de contraseña, restablecimiento de contraseña y actualización de información de perfil. - Se implementaron pruebas de registro y autenticación de dos factores. - Se crearon pruebas para actualización de contraseñas y eliminación de cuentas.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,24 +3,32 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
-        'id',
-        'dni',
+        'name',
         'email',
         'password',
     ];
@@ -28,11 +36,22 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
     ];
 
     /**
@@ -46,13 +65,5 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    /**
-     * Get the profile associated with the user.
-     */
-    public function profile() 
-    {
-        return $this->hasOne(UserProfile::class);
     }
 }
