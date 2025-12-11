b44727310a94c22faa3efcb81852b97a91833be2
Ranses Jiménez Domínguez <hiperiom412@gmail.com>
2025-12-10 23:46:45 -0400

    feat: agregar propiedades de usuario compartidas en la respuesta de Inertia

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,11 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
-            //
+            [
+                'appName' => config('app.name'),
+                'user.roles' => $request->user() ? $request->user()->roles->pluck('name') : [],
+                'user.permissions' => $request->user() ? $request->user()->getPermissionsViaRoles()->pluck('name') : [],
+            ],
         ];
     }
 }
